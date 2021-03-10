752cac19bad92ea50f9abb495a7b7f7b9a841abf
TheIzvorg <izvorg@yandex.ru>
2021-03-10 16:40:36 +0300

    Добавьте файлы проекта.

---
+++ CollegeCheck/AuthWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CollegeCheck
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Start_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtbox_Login.Text.Equals("isp95vb") && pswdbox_Password.Password.Equals("college"))
+            {
+                CheckWindow checkWindow = new CheckWindow();
+                checkWindow.Show();
+                this.Close();
+            }
+        }
+
+        private void btn_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = 
+                MessageBox.Show("Вы уверены, что хотите выйти?","Вы уверены?",MessageBoxButton.YesNo,MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
